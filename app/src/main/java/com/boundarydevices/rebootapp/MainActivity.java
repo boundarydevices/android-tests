1676fc48750d0c00d8656bf1550004083327bc0d
Gary Bisson <gary.bisson@boundarydevices.com>
2015-04-30 22:29:35 +0200

    Add a reboot button that triggers a PowerManager.reboot() call
    Thus needing android.permission.REBOOT which is only available to system
    applications.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- app/src/main/java/com/boundarydevices/rebootapp/MainActivity.java
+++ app/src/main/java/com/boundarydevices/rebootapp/MainActivity.java
@@ -1,13 +1,25 @@
 package com.boundarydevices.rebootapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends Activity {
+
+    private String TAG = "BoundaryTestApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doReboot(View view) {
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        Log.v(TAG, "Now rebooting...");
+        pm.reboot(null);
     }
 }
