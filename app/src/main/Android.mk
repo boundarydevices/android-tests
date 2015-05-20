50e518a198acbbe9f7c0e4a182c44f8abf9216fe
Gary Bisson <gary.bisson@boundarydevices.com>
2015-05-20 10:29:25 +0200

    Add Android.mk to build without studio
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

---
+++ app/src/main/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, java)
+
+LOCAL_PACKAGE_NAME := SuApp
+
+include $(BUILD_PACKAGE)
