56ff4469b229f9ec9bec103bdd2f9d91e53c2e45
Gary Bisson <gary.bisson@boundarydevices.com>
2016-01-27 11:59:00 +0100

    TestEthernet: remove download but display properties
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- app/src/main/java/com/boundarydevices/platformtesting/TestEthernet.java
+++ app/src/main/java/com/boundarydevices/platformtesting/TestEthernet.java
@@ -3,28 +3,23 @@ package com.boundarydevices.platformtesting;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class TestEthernet extends Activity {
 
     private final String TAG = this.getClass().getSimpleName();
     private Context mContext;
-    private ProgressBar mEthProgress;
     private TextView mEthText;
-    private DownloadFilesTask mDlTask;
+    private ConnectivityManager mConnMgr;
+    private Button mEthYes;
+    private Button mEthNo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,98 +33,73 @@ public class TestEthernet extends Activity {
         /* Initialize the result */
         TestResults.addResult(TAG, TestResults.TEST_RESULT_FAILED);
 
-        mEthText = (TextView) findViewById(R.id.eth_text);
-        mEthText.setText("Downloading a 10MB file...");
-        mEthProgress = (ProgressBar) findViewById(R.id.eth_progress);
+        mConnMgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        final Button button_eth_skip = (Button) findViewById(R.id.button_eth_skip);
-        button_eth_skip.setOnClickListener(new View.OnClickListener() {
+        mEthText = (TextView) findViewById(R.id.eth_text);
+        mEthText.setText("Please connect the platform to the Network...\n");
+
+        mEthYes = (Button) findViewById(R.id.button_eth_yes);
+        mEthYes.setVisibility(View.INVISIBLE);
+        mEthYes.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.v(TAG, "button_eth_skip");
-                mDlTask.cancel(true);
+                Log.v(TAG, "button_eth_yes");
+                TestResults.addResult(TAG, TestResults.TEST_RESULT_SUCCESS);
                 Intent intent = new Intent(mContext, TestAudio.class);
                 startActivity(intent);
             }
         });
 
-        /* Download a file a roughly 10MB */
-        mDlTask = new DownloadFilesTask();
-        mDlTask.execute("https://storage.googleapis.com/boundarydevices.com/bdsl_ethernet_debugging.avi");
+        mEthNo = (Button) findViewById(R.id.button_eth_no);
+        mEthNo.setVisibility(View.INVISIBLE);
+        mEthNo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.v(TAG, "button_eth_no");
+                Intent intent = new Intent(mContext, TestAudio.class);
+                startActivity(intent);
+            }
+        });
+
+        final Button button_eth_skip = (Button) findViewById(R.id.button_eth_skip);
+        button_eth_skip.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.v(TAG, "button_eth_skip");
+                Intent intent = new Intent(mContext, TestAudio.class);
+                startActivity(intent);
+            }
+        });
+
+        new WaitingTask().execute();
     }
 
-    private class DownloadFilesTask extends AsyncTask<String, Integer, String> {
-        protected String doInBackground(String... sUrl) {
-            InputStream input = null;
-            OutputStream output = null;
-            HttpURLConnection connection = null;
-            try {
-                URL url = new URL(sUrl[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-
-                // expect HTTP 200 OK, so we don't mistakenly save error report
-                // instead of the file
-                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                    return "Server returned HTTP " + connection.getResponseCode()
-                            + " " + connection.getResponseMessage();
-                }
-
-                // this will be useful to display download percentage
-                // might be -1: server did not report the length
-                int fileLength = connection.getContentLength();
-
-                // download the file
-                input = connection.getInputStream();
-                output = new FileOutputStream(mContext.getFilesDir() + "/file.temp");
-
-                byte data[] = new byte[4096];
-                long total = 0;
-                int count;
-                while ((count = input.read(data)) != -1) {
-                    // allow canceling with back button
-                    if (isCancelled()) {
-                        input.close();
-                        return null;
-                    }
-                    total += count;
-                    // publishing the progress....
-                    if (fileLength > 0) // only if total length is known
-                        publishProgress((int) (total * 100 / fileLength));
-                    output.write(data, 0, count);
-                }
-            } catch (Exception e) {
-                return e.toString();
-            } finally {
+    private class WaitingTask extends AsyncTask<Void, Integer, Void> {
+        protected Void doInBackground(Void... values) {
+            for (;;) {
+                if (mConnMgr.getActiveNetworkInfo() != null)
+                    break;
                 try {
-                    if (output != null)
-                        output.close();
-                    if (input != null)
-                        input.close();
-                } catch (IOException ignored) {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-
-                if (connection != null)
-                    connection.disconnect();
             }
             return null;
         }
 
-        protected void onProgressUpdate(Integer... progress) {
-            Log.v(TAG, "Ethernet download progress: " + progress[0].toString());
-            mEthProgress.setProgress(progress[0]);
+        protected void onProgressUpdate(Integer... values) {
         }
 
-        protected void onPostExecute(String result) {
-            if (result == null) {
-                Log.v(TAG, "Download complete!");
-                mEthText.setText("Download complete!");
-                TestResults.addResult(TAG, TestResults.TEST_RESULT_SUCCESS);
-            } else {
-                Log.v(TAG, "Download failed: " + result);
-                mEthText.setText("Download failed: " + result);
-            }
-            Intent intent = new Intent(mContext, TestAudio.class);
-            startActivity(intent);
+        protected void onPostExecute(Void result) {
+            mEthText.setText("Ethernet settings:\n\n");
+            Network network = mConnMgr.getAllNetworks()[0];
+            mEthText.append("MAC:  " + mConnMgr.getNetworkInfo(network).getExtraInfo().toString() + "\n");
+            mEthText.append("IP:  " + mConnMgr.getLinkProperties(network).getLinkAddresses().toString() + "\n");
+            mEthText.append("DNS:  " + mConnMgr.getLinkProperties(network).getDnsServers().toString() + "\n");
+            mEthText.append("Routes:  " + mConnMgr.getLinkProperties(network).getRoutes().toString() + "\n");
+            mEthText.append("Status:  " + mConnMgr.getNetworkInfo(network).getState().toString() + "\n");
+            mEthText.append("\nIs this correct?\n");
+
+            mEthYes.setVisibility(View.VISIBLE);
+            mEthNo.setVisibility(View.VISIBLE);
         }
     }
 }
