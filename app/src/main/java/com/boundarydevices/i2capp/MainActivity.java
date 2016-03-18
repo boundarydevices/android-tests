21bb7b31515d6ac799251cc51ae66385b406def1
Gary Bisson <gary.bisson@boundarydevices.com>
2016-03-18 19:11:30 +0100

    Implement basic I2C read/write functions
    Works for both 1-Byte and Block transfers.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- app/src/main/java/com/boundarydevices/i2capp/MainActivity.java
+++ app/src/main/java/com/boundarydevices/i2capp/MainActivity.java
@@ -2,12 +2,46 @@ package com.boundarydevices.i2capp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    /* ISL1208 definitions */
+    private final int I2C_RTC_BUS = 0;
+    private final int I2C_RTC_ADDR = 0x6F;
+    private final int I2C_RTC_MONTH = 0x4;
+
+    private TextView mTextView;
+    private int mFileDesc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int result;
+        int backup;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = (TextView)findViewById(R.id.MainTextView);
+
+        /* Create i2c device object */
+        I2CDevice device;
+        device = new I2CDevice();
+
+        /* Open ISL1208 device */
+        mFileDesc = device.open(I2C_RTC_BUS, I2C_RTC_ADDR);
+        mTextView.setText("Opening device on bus " + I2C_RTC_BUS + " @0x" +
+                Integer.toHexString(I2C_RTC_ADDR) + "...\n");
+
+        /* Read the byte at offset 0x4 which is the month */
+        result = device.readByte(mFileDesc, I2C_RTC_MONTH);
+        if (result < 0) {
+            mTextView.append("Error read: " + result);
+            return;
+        }
+        mTextView.append("Received month value: " + result + "\n");
+        backup = result;
+
+        /* Closing the device file descriptor */
+        device.close(mFileDesc);
     }
 }
--- app/src/main/jni/i2c-android-jni.c
+++ app/src/main/jni/i2c-android-jni.c
@@ -1,28 +1,76 @@
 #include <jni.h>
-#include <stddef.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <android/log.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+
+/* Define Log macros */
+#define  LOG_TAG    "i2c-android-jni"
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+int smbus_xfer(int fd, int read_write, int command, int size, union i2c_smbus_data *buffer)
+{
+    struct i2c_smbus_ioctl_data data;
+
+    data.read_write = read_write;
+    data.command = command;
+    data.size = size;
+    data.data = buffer;
+
+    return ioctl(fd, I2C_SMBUS, &data);
+}
 
 JNIEXPORT jint JNICALL
 Java_com_boundarydevices_i2capp_I2CDevice_open(JNIEnv *env, jobject instance, jint bus,
                                                jint address) {
+    char dev[16];
+    int fd;
+    int ret;
 
-    // TODO
+    sprintf(dev, "/dev/i2c-%d", bus);
+    LOGD("Opening %s", dev);
+    fd = open(dev, O_RDWR);
+    if (fd < 0) {
+        LOGE("Couldn't open %s: %s", dev, strerror(errno));
+        return fd;
+    }
 
+    LOGD("Setting address to %#02x", address);
+    ret = ioctl(fd, I2C_SLAVE_FORCE, address);
+    if (ret != 0) {
+        LOGE("Couldn't set slave address: %s", strerror(errno));
+        return -1;
+    }
+
+    return fd;
 }
 
 JNIEXPORT jint JNICALL
 Java_com_boundarydevices_i2capp_I2CDevice_readByte(JNIEnv *env, jobject instance, jint fd,
                                                    jint offset) {
+    int ret;
+    union i2c_smbus_data data;
 
-    // TODO
+    ret = smbus_xfer(fd, I2C_SMBUS_READ, offset, I2C_SMBUS_BYTE_DATA, &data);
+    if (ret < 0) {
+        LOGE("smbus_xfer read issue: %s", strerror(errno));
+        return ret;
+    }
 
+    return (jint)data.byte;
 }
 
 JNIEXPORT jint JNICALL
 Java_com_boundarydevices_i2capp_I2CDevice_writeByte(JNIEnv *env, jobject instance, jint fd,
                                                     jint offset, jbyte value) {
+    union i2c_smbus_data data;
 
-    // TODO
+    data.byte = value;
 
+    return smbus_xfer(fd, I2C_SMBUS_WRITE, offset, I2C_SMBUS_BYTE_DATA, &data);
 }
 
 JNIEXPORT jint JNICALL
@@ -30,9 +78,20 @@ Java_com_boundarydevices_i2capp_I2CDevice_readBlock(JNIEnv *env, jobject instanc
                                                     jint offset, jbyteArray buffer_, jint count) {
     jbyte *buffer = (*env)->GetByteArrayElements(env, buffer_, NULL);
 
-    // TODO
+    union i2c_smbus_data data;
+    int i;
+    int ret;
+
+    ret = smbus_xfer(fd, I2C_SMBUS_READ, offset, I2C_SMBUS_BLOCK_DATA, &data);
+    if (ret < 0)
+        return ret;
+
+    for (i = 1; i <= data.block[0]; i++)
+        buffer[i-1] = data.block[i];
 
     (*env)->ReleaseByteArrayElements(env, buffer_, buffer, 0);
+
+    return data.block[0];
 }
 
 JNIEXPORT jint JNICALL
@@ -40,14 +99,25 @@ Java_com_boundarydevices_i2capp_I2CDevice_writeBlock(JNIEnv *env, jobject instan
                                                      jint offset, jbyteArray buffer_, jint count) {
     jbyte *buffer = (*env)->GetByteArrayElements(env, buffer_, NULL);
 
-    // TODO
+    union i2c_smbus_data data;
+    int i;
+
+    if (count > I2C_SMBUS_BLOCK_MAX) {
+        LOGI("Reduce count %d to %d", count, I2C_SMBUS_BLOCK_MAX);
+        count = I2C_SMBUS_BLOCK_MAX;
+    }
+
+    for (i = 1; i <= count; i++)
+        data.block[i] = buffer[i-1];
+    data.block[0] = count;
 
     (*env)->ReleaseByteArrayElements(env, buffer_, buffer, 0);
+
+    return smbus_xfer(fd, I2C_SMBUS_WRITE, offset, I2C_SMBUS_BLOCK_DATA, &data);
+
 }
 
 JNIEXPORT void JNICALL
 Java_com_boundarydevices_i2capp_I2CDevice_close(JNIEnv *env, jobject instance, jint fd) {
-
-    // TODO
-
+    close(fd);
 }
