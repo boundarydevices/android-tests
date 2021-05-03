c54dac9a804ad2354eeeede246c81cf4297d5ff1
Gary Bisson <gary.bisson@boundarydevices.com>
2021-05-03 18:49:09 +0200

    app: make main view fullscreen by default
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- app/src/main/java/com/boundarydevices/kioskapp/MainActivity.java
+++ app/src/main/java/com/boundarydevices/kioskapp/MainActivity.java
@@ -4,12 +4,28 @@ package com.boundarydevices.kioskapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private View mContentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContentView = findViewById(R.id.fullscreen_content);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mContentView != null) {
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
     }
 }
